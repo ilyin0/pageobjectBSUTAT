2ced355c4a83822ec415b3889d66f0d59dc7aae3
ilyin0 <ilyindeveloper@gmail.com>
2020-12-21 12:15:44 +0300

    Add waitTime(long) method, resetImplicitlyTimeout, setImplicitlyTimeout methods

--- src/test/java/pages/AbstractPage.java
+++ src/test/java/pages/AbstractPage.java
@@ -1,17 +1,51 @@
 package pages;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
 
 public abstract class AbstractPage {
 
     protected WebDriver driver;
+    protected final int WAIT_TIMEOUT_SECONDS = 10;
 
     protected abstract AbstractPage openPage();
-    protected final int WAIT_TIMEOUT_SECONDS = 10;
+
+    public void closePage() {
+        driver.quit();
+    }
 
     protected AbstractPage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver, this);
     }
+
+    protected boolean isJavascriptExecuted() {
+        return ((JavascriptExecutor) driver).executeScript("return document.readyState").equals("complete");
+    }
+
+    protected AbstractPage waitJavascriptIsExecuted(long timeout) {
+        new WebDriverWait(driver, timeout).until(d->isJavascriptExecuted());
+        return this;
+    }
+
+    protected void setImplicitlyTimeouts(long time, TimeUnit timeUnit) {
+        driver.manage().timeouts().implicitlyWait(time, timeUnit);
+    }
+
+    protected void resetImplicitlyTimeouts() {
+        driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+    }
+
+    protected void waitTime(long timeInMillis) {
+        try {
+            Thread.sleep(timeInMillis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
